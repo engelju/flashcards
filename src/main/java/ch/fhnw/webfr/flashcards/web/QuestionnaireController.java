22df3a8c1553a1d27a2eaaad8b8bf6bdbd4589a7
Julie Engel <julie.engel@mail.com>
2022-11-23 00:52:54 +0100

    le04-ab7: integrate template views with thymeleaf

--- src/main/java/ch/fhnw/webfr/flashcards/web/QuestionnaireController.java
+++ src/main/java/ch/fhnw/webfr/flashcards/web/QuestionnaireController.java
@@ -1,15 +1,10 @@
 package ch.fhnw.webfr.flashcards.web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 import java.util.Optional;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,38 +20,17 @@ public class QuestionnaireController {
     private QuestionnaireRepository questionnaireRepository;
     
     @RequestMapping(method = RequestMethod.GET)
-    public void findAll(HttpServletResponse response, HttpServletRequest request) throws IOException {
-        List<Questionnaire> questionnaires = questionnaireRepository.findAll();
-		
-        PrintWriter writer = response.getWriter();
-        writer.append("<html><head><title>Example</title></head><body>");
-        writer.append("<h3>Fragebögen</h3>");
-        
-        for (Questionnaire questionnaire : questionnaires) {
-            String url = request.getRequestURL() + "/" + questionnaire.getId().toString();
-            writer.append("<p><a href='" + response.encodeURL(url) + "'>" + questionnaire.getTitle() + "</a></p>");
-        }
-        
-        writer.append("</body></html>");
+    public String findAll(Model model) {
+		model.addAttribute("questionnaires", questionnaireRepository.findAll());
+        return "questionnaires/list";
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
-    // @RequestMapping(value = "/{id}",  method = RequestMethod.GET)
-    public void findById(@PathVariable String id, HttpServletResponse response, HttpServletRequest request) throws IOException {
+    public String findById(@PathVariable String id, Model model) {
         Optional<Questionnaire> questionnaire = questionnaireRepository.findById(id);
-		
-        PrintWriter writer = response.getWriter();
-        writer.append("<html lang='en'><head><title>Example</title></head><body>");
-        writer.append("<h2>Questionnaire</h2>");
-        
         if (questionnaire.isPresent()) {
-            writer.append("<h3>" + questionnaire.get().getTitle() + "</h3>");
-            writer.append("<p>" + questionnaire.get().getDescription() + "</p>");	
-        } else {
-            writer.append("<p><em>no questionnaire found</em></p>");
+            model.addAttribute("questionnaire", questionnaire.get());
         }
-        
-        writer.append("<a href='" + request.getContextPath() + "/questionnaires'>Back</a>");
-        writer.append("</body></html>");
+        return "questionnaires/show";
     }
 }
